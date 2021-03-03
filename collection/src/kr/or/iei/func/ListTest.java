2a6dea50909e2990bb996db73e8357a8f2953812
EunhyeonKang <rkddmsgus1@daum.net>
2021-03-03 16:42:21 +0900

    0303 collection(List, map)

---
+++ collection/src/kr/or/iei/func/ListTest.java
@@ -0,0 +1,60 @@
+package kr.or.iei.func;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class ListTest {
+	public void listTest1() {
+		String[] str = new String[3];
+		str[0] = "Hi";
+		str[1] = "Hello";
+		str[2] = "안녕하세요";
+		System.out.println("배열 내용 출력");
+		for(int i=0;i<str.length;i++) {
+			System.out.println((i+1)+". "+ str[i]);
+		}
+		ArrayList<String> list = new ArrayList<String>(); 
+		System.out.println("listsize : "+list.size());
+		list.add("Hi");		//list의 가장 뒤에 Hi를 추가
+		System.out.println("listsize : "+list.size());
+		list.add("Hello");
+		System.out.println("listsize : "+list.size());
+		list.add("안녕하세요");
+		System.out.println("listsize : "+list.size());
+		System.out.println("리스트 내용 출력");
+		for(int i=0;i<list.size();i++) {
+			System.out.println((i+1)+". "+ list.get(i));
+		}
+	}
+	public void listTest2() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(50);				//추가
+		list.add(20);
+		list.add(10);
+		list.add(16);
+		list.add(24);
+		list.remove(2);				//삭제
+		for(int i=0;i<list.size();i++) {
+			System.out.println((i+1)+". "+list.get(i));
+		}
+		int num =list.set(3, 100);	//해당인덱스의 값을 변경하고, 변경 전 값을 리턴
+		System.out.println("num : "+num);
+		for(int i=0;i<list.size();i++) {
+			System.out.println((i+1)+". "+list.get(i));
+		}
+		System.out.println("리스트 정렬");
+		Collections.sort(list);
+		for(int i=0;i<list.size();i++) {
+			System.out.println((i+1)+". "+list.get(i));
+		}
+		System.out.println("리스트 역순");
+		Collections.reverse(list);
+		for(int i=0;i<list.size();i++) {
+			System.out.println((i+1)+". "+list.get(i));
+		}
+		System.out.println(list);
+		list.clear();
+		System.out.println(list);
+	}
+}
---
+++ collection/src/kr/or/iei/run/Start.java
@@ -0,0 +1,11 @@
+package kr.or.iei.run;
+
+import kr.or.iei.func.ListTest;
+
+public class Start {
+	public static void main(String[] args) {
+		ListTest st = new ListTest();
+//		st.listTest1();
+		st.listTest2();
+	}
+}
