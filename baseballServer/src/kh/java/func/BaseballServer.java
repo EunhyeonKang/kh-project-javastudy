bab1980ed5e6ed39b41309d91a7650aafd332a54
EunhyeonKang <rkddmsgus1@daum.net>
2021-03-11 02:14:30 +0900

    0311 baseballServer, baseballClient 마무리

--- baseballServer/src/kh/java/func/BaseballServer.java
+++ baseballServer/src/kh/java/func/BaseballServer.java
@@ -24,57 +24,84 @@ public class BaseballServer {
 		int cnt = 0;
 		try {
 			server = new ServerSocket(serverPort);
-
-			System.out.println("클라이언트의 접속을 기다립니다.....");
-			Socket socket = server.accept();
-			System.out.println("클라이언트가 접속했습니다.");
-			OutputStream out = socket.getOutputStream();
-			InputStream in = socket.getInputStream();
-			dos = new DataOutputStream(out);
-			dis = new DataInputStream(in);
-			ArrayList<Integer> array = new ArrayList<Integer>();
-			ArrayList<Integer> array1 = new ArrayList<Integer>();
-			System.out.println("<<<<<BaseBall Game>>>>>");
-			System.out.print("서버 숫자 : ");
-			for (int i = 0; i < 3; i++) {
-				int rNum = r.nextInt(10);
-				array.add(rNum);
-				System.out.print(array.get(i) + " ");
-			}
-			System.out.println();
 			while (true) {
-				cnt++;
+				s = 0;
+				b = 0;
+				System.out.println("클라이언트의 접속을 기다립니다.....");
+				Socket socket = server.accept();
+				System.out.println("클라이언트가 접속했습니다.");
+				OutputStream out = socket.getOutputStream();
+				InputStream in = socket.getInputStream();
+				dos = new DataOutputStream(out);
+				dis = new DataInputStream(in);
+				int[] array = new int[3];
+				int[] array1 = new int[3];
+				System.out.println("<<<<<BaseBall Game>>>>>");
+
+				for (int i = 0; i < 3; i++) {
+					int rNum = r.nextInt(10);
+					array[i] = rNum;
+
+				}
+
+				for (int i = 0; i < array.length; i++) {
+					// 중복된 값인지
+					int rNum1 = r.nextInt(10);
+					// 입력 범위가 정상인 경우
+					boolean bool = true;
+					for (int j = 0; j < i; j++) {
+						if (rNum1 == array[j]) {
+							i--;
+							bool = false;
+							break;
+						}
+					}
+					if (bool) {
+						array[i] = rNum1;
+					}
+				}
+				System.out.print("서버 숫자 : ");
+				for (int i = 0; i < array.length; i++) {
+					System.out.print(array[i] + " ");
+				}
+				System.out.println();
+
 				System.out.println("<<<<<Game Start>>>>>");
 				while (true) {
 					System.out.print("클라이언트가 입력한 수 -> ");
-					for (int i = 0; i < array.size(); i++) {
+					for (int i = 0; i < array.length; i++) {
 						int cNum = dis.readInt();
-						array1.add(cNum);
-						System.out.print(array1.get(i) + " ");
-						for (int j = 0; j < array1.size(); j++) {
-							if (array.get(i) == array1.get(j) && i == j) {
-								s++;
-							} else if (array.get(i) == array1.get(j) && i != j) {
-								b++;
+						array1[i] = cNum;
+						System.out.print(array1[i] + " ");
+
+					}
+					for (int i = 0; i < array.length; i++) {
+						for (int j = 0; j < array1.length; j++) {
+							if (array[i] == array1[j]) {
+								if (i == j) {
+									s++;
+								} else {
+									b++;
+								}
 							}
+
 						}
 					}
-					array1.clear();
 					System.out.println();
 					System.out.println(s + "스트라이크 " + b + "볼");
-					
-					dos.writeInt(s);
-					dos.writeInt(b);
-					
-					String cMsg = dis.readUTF();
-					if(cMsg.equals("exit")) {
+					cnt++;
+					if (s == 3 || cnt == 9) {
 						System.out.println("3스크라이크. 정답 현재 클라이언트와의 접속을 종료합니다.");
+						dos.writeInt(s);
+						dos.writeInt(b);
 						break;
 					}
+					dos.writeInt(s);
+					dos.writeInt(b);
 					s = 0;
 					b = 0;
 				}
-				break;
+
 			}
 
 		} catch (IOException e) {
